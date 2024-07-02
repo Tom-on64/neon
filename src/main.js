915d7d9e0c308c2d78ebcf6a8642defda07415e8
Tom-on64 <kubecekt@gmail.com>
2024-07-02 10:17:02 +0200

    Added command line argument parsing

---
+++ src/main.js
@@ -0,0 +1,46 @@
+const HELP_MSG = "Usage: neon <file> [-o <file>] [-h] [-v]";
+const VERSION = "0.0.1";
+
+const argv = [...process.argv];
+let infile = null;
+let outfile = "out.s"; // We will be outputting assembly for now
+
+if (argv[0].includes("node")) { 
+    argv.shift(); // Gets rid of node.js arg and sets up expected state
+}
+
+if (argv.length < 2) {
+    console.log(HELP_MSG);
+    process.exit(1);
+}
+
+// Parse command line args
+for (let i = 1; i < argv.length; i++) {
+    const arg = argv[i];
+
+    if (arg[0] === '-') {
+        switch (arg) {
+            case "-h":
+                console.log(HELP_MSG);
+                process.exit(0);
+                break;
+            case "-v":
+                console.log(`Neon ${VERSION}`);
+                process.exit(0);
+                break;
+            case "-o":
+                if (++i === argv.length) {
+                    console.log("Expected file after '-o'!");
+                    process.exit(1);
+                }
+                outfile = argv[i];
+                break;
+        }
+    } else {
+        infile = arg;
+    }
+}
+
+// TODO: The actuall compiling part of the compiler
+console.log(`${infile} -> ${outfile}`);
+
